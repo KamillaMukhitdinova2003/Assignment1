132146d7bd0b8b84bbb8dacd409dca4c03346faa
Kamilla <kamillamuhitdinova508@gmail.com>
2023-12-26 09:55:29 +0600

    some changes

---
+++ src/byMyself/First_Program.java
@@ -0,0 +1,7 @@
+package byMyself;
+public class First_Program {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming");
+        First_Program blue;
+    }
+}
